6ded50a032ba953afbfdf3167ab4a9282ea49c6d
Masachusets <apofeoz@tut.by>
2022-06-12 14:09:10 +0300

    some small fixes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from habr_scraping import find_articles_by_keywords
 
-KEY_WORDS = ['дизайн', 'фото', 'web', 'python']
+KEYWORDS = ['дизайн', 'фото', 'web', 'python']
 
 if __name__ == '__main__':
-    print(*find_articles_by_keywords(KEY_WORDS), sep='\n')
+    print(*find_articles_by_keywords(KEYWORDS), sep='\n')
